08e122bed319a44d373eac957afb18c5ea4a4cd1
Jose Alzuri <jalzu1007@gmail.com>
2023-09-13 17:56:02 -0400

    Added config connection and server connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
+require('dotenv').config(); 
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booksearch_DB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
